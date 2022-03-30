ca584ccae34f1bd1bed7b18eafb7b1ae015d75ab
Mark Stevens <markspstevens@yahoo.co.uk>
2022-03-30 11:01:30 +0200

    env template

--- src/FireBase/firebase.js
+++ src/FireBase/firebase.js
@@ -10,6 +10,7 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID 
+
 }
 
 const app = initializeApp(firebaseConfig)
